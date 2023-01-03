c1dc3e3e8077a4a3e15ee8cfd261f8d602a502c3
khalilDimassi <dimassi0khalil@gmail.com>
2023-01-03 03:41:24 +0100

    added bunch of TODOs

--- GeneralAuth/urls.py
+++ GeneralAuth/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('settings/<int:pk>/edit/', views.edit_settings_view, name='edit-settings'),
     path('settings/<int:pk>/password/', views.edit_password_view, name='edit-password'),
+
+    # TODO: Delete account
+    # path('settings/<int:pk>/delete/', views.delete_account_view, name='delete-account'),
+
+    # TODO: Reset password
+    # path('reset-password/', views.reset_password_view, name='reset-password'),
 ]
