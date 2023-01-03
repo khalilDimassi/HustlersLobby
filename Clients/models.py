25f935896f7ea12b14a8ff05acc055875689b742
khalilDimassi <dimassi0khalil@gmail.com>
2023-01-03 11:53:03 +0100

    added delete account feature

--- Clients/models.py
+++ Clients/models.py
@@ -13,7 +13,7 @@ class ClientProfile(models.Model):
     establishment = models.CharField(max_length=100, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True, default=timezone.now)
     profile_picture = models.ImageField(upload_to='imgs/profile_imgs', null=True, blank=True)
-    bio = models.TextField(max_length=500, null=True, blank=True)
+    bio = models.TextField(max_length=2000, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     fb_link = models.CharField(max_length=200, null=True, blank=True)
